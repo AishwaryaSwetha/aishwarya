c39ce5943f958da97929b47910e5f8d6cb7a9016
aishwe <swetaaishwarya1@gmail.com>
2017-02-27 15:20:31 +0530

    Add files via upload

---
+++ Friends.java
@@ -0,0 +1,13 @@
+import package java.util.*;
+class Friends
+{
+public static void main(String args[])
+{
+int t;
+System.out.println("enter the no of tasks");
+Scanner sc=new Scanner(System.in);
+t=sc.nextInt();
+System.out.println("number of tasks are"+t);
+}
+}
+
