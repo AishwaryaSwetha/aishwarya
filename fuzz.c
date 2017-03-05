4ea8e68dcd9d87bb325c8dee6bbd300902ca2c1d
aishwe <swetaaishwarya1@gmail.com>
2017-03-05 21:09:43 +0530

    Add files via upload

---
+++ fuzz.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+    
+int t,x=0,n,i;
+    
+scanf("%d",&t);
+
+    //printf("no of test cases are:%d",t);
+    
+printf("\n");
+ 
+while(x<t)
+
+{
+        
+scanf("%d",&n);
+
+       // printf("the number is:%d",n);
+printf("\n");
+        
+for(i=1;i<=n;i++)
+ {
+           
+ if((i%3==0)&&(i%5==0))
+ {
+                
+printf("fizzbuzz");
+               
+ printf("\n");
+      
+      }
+
+  else if(i%3==0)
+            
+{
+
+printf("fizz");
+ printf("\n");
+  }
+         
+ else if(i%5==0)
+ 
+ {
+             
+ printf("buzz");
+
+ printf("\n");
+         
+ }
+          
+else
+          
+{
+          
+printf("%d",i);
+          
+printf("\n");
+          
+}
+     
+      
+}
+      
+x++;
+
+}
+ 
+return 0;
+
+}
